0856d93b9b6ac444edbfa6dff4ab81ad920f1837
Paul Sommer <paul@sommers.dev>
2026-02-17 12:35:55 +0100

    classroom Todo 1 & 2

--- src/at/htlhl/sew2/array2d/classroom/Main.java
+++ src/at/htlhl/sew2/array2d/classroom/Main.java
@@ -4,30 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-/*
-   TODO 1:
-   Prompt: "Erstelle eine csv Datei, welche 24 Schüler und Schülerinnen enthält.
-   Die Werte pro Schüler bzw. Schülerin sind Katalognummer, Vorname, Nachname, Gender (M, F oder D) und
-   das Geburtsdatum (zweite Hälfte 2009 und erste Hälfte 2010) im Format yyyy-MM-dd.
-   Die Werte sollen durch Strichpunkte getrennt sein und zum Download bereitgestellt werden.
-   Sortiere die Einträge zuerst nach dem Nachnamen, wenn die Nachnamen gleich sind
-   dann zusätzlich nach dem Vornamen und wenn die Vornamen auch gleich sind auch
-   noch nach dem Geburtsdatum."
-
-   Speichere diese csv-Datei im Ordner von SeasonProject ab.
-
-   Kopiere den Namen mit deiner Katalognummer ans Ende und passe die Katalognummer von diesem Eintrag
-   an (= Katalognummer 25).
- */
-
 public class Main {
     void main() throws IOException {
         ClassRoom classRoom = new ClassRoom(3, 8);
 
         readStudentsFromFile(classRoom);
 
-        // TODO 2: Weise dem Namen mit deiner Katalognummer einen Platz in der 2. Reihe zu
-        //         Achtung: Hier muss der Indexwert verwendet werden, welcher Katalognummer - 1 ist.
         classRoom.assignSeat(classRoom.findStudentAtIndex(20), 1, 5);
 
         // TODO 3: Weise weiteren zwei Namen einen Platz in der ersten Reihe zu und
