93a643004df826826a0578ec19980ce87b45e91f
Paul Sommer <paul@sommers.dev>
2026-03-10 12:03:51 +0100

    implement static Gas Station Prices Display

---
+++ src/at/htlhl/sew2/format/GasStationDisplay.java
@@ -0,0 +1,7 @@
+void main() {
+    IO.println("-----------------------------------");
+    IO.println(String.format("%27s", "Gas Station Prices"));
+    IO.println("-----------------------------------");
+    IO.println(String.format("%-20s %10s", "Fuel Type", "Price (€/1)"));
+    IO.println("-----------------------------------");
+}
