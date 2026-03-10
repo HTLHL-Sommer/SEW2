892e95928ddccb9811973f635d29d55b25a52402
Paul Sommer <paul@sommers.dev>
2026-03-10 12:09:17 +0100

    add a fuelList to GasStationDisplay

--- src/at/htlhl/sew2/format/GasStationDisplay.java
+++ src/at/htlhl/sew2/format/GasStationDisplay.java
@@ -1,4 +1,13 @@
+import at.htlhl.sew2.format.Fuel;
+
 void main() {
+    ArrayList<Fuel> fuelList = new ArrayList<>();
+
+    fuelList.add(new Fuel("Diesel", 1.959));
+    fuelList.add(new Fuel("Super 95", 1.729));
+    fuelList.add(new Fuel("V-Power° 100", 1.929));
+    fuelList.add(new Fuel("V-Power° Diesel", 2.159));
+
     IO.println("-----------------------------------");
     IO.println(String.format("%27s", "Gas Station Prices"));
     IO.println("-----------------------------------");
