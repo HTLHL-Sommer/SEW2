1515e3c1a04592e53483ffe9a9ef65b20336d33b
Paul Sommer <paul@sommers.dev>
2026-03-17 12:52:29 +0100

    implement valueOf and toString

--- src/at/htlhl/sew2/format/Fuel.java
+++ src/at/htlhl/sew2/format/Fuel.java
@@ -16,4 +16,14 @@ public class Fuel {
     public double getPrice() {
         return this.price;
     }
+
+    public static Fuel valueOf(String input) {
+        String[] parts = input.split(";");
+        return new Fuel(parts[0], Double.parseDouble(parts[1]));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name: %-15s Price: %6.3f €", this.name, this.price);
+    }
 }
--- src/at/htlhl/sew2/format/GasStationDisplay.java
+++ src/at/htlhl/sew2/format/GasStationDisplay.java
@@ -9,6 +9,9 @@ void main() {
     fuelList.add(new Fuel("V-Power° 100", 1.929));
     fuelList.add(new Fuel("V-Power° Diesel", 2.159));
 
+    String userInput = IO.readln("Eigene Treibstoffsorte inkl. Preis <fuel>;<price>: ");
+    Fuel userFuel = Fuel.valueOf(userInput);
+
     IO.println("-----------------------------------");
     IO.println(String.format("%27s", "Gas Station Prices"));
     IO.println("-----------------------------------");
@@ -17,8 +20,11 @@ void main() {
     for (Fuel fuel : fuelList) {
         IO.println(String.format("%-20s %10.3f", fuel.getName(), fuel.getPrice()));
     }
+    IO.println(String.format("%-20s %10.3f", userFuel.getName(), userFuel.getPrice()));
     IO.println("-----------------------------------");
 
     IO.println();
     IO.println(String.format("Total fuel types: %d", fuelList.size()));
+
+    IO.println(userFuel.toString());
 }
