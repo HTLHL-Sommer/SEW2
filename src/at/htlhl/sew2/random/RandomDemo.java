85225bb4df369031eda59671a4764da41f13a6a2
Paul Sommer <paul@sommers.dev>
2026-04-14 11:47:37 +0200

    init RandomDemo

---
+++ src/at/htlhl/sew2/random/RandomDemo.java
@@ -0,0 +1,10 @@
+package at.htlhl.sew2.random;
+
+import java.util.Random;
+
+public class RandomDemo {
+    void main() {
+        IO.println("Einfache Zufallszahlen (ganzzahlig) mit der Klasse Random.");
+        Random random = new Random();
+    }
+}
